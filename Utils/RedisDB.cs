4272215e4f66ad4b4f1c148afd4ce79787630fa5
DevJulio99 <166440780+DevJulio99@users.noreply.github.com>
2025-04-23 16:32:15 -0500

    Merge pull request #8 from DevJulio99/feature/captcha-login
    Feature/captcha login

---
+++ Utils/RedisDB.cs
@@ -0,0 +1,38 @@
+﻿using StackExchange.Redis;
+
+namespace MyPortalStudent.Utils
+{
+    public class RedisDB: IRedisDB
+    {
+        private readonly IConnectionMultiplexer _redis;
+        private readonly IDatabase _database;
+        public RedisDB(IConnectionMultiplexer redis) {
+            _redis = redis;
+            _database = redis.GetDatabase();
+        }
+        public void SetString(string key, string value, TimeSpan? expiry = null) 
+        {
+            _database.StringSet(key, value, expiry);
+        }
+        public string GetString(string key)
+        {
+            return _database.StringGet(key);
+        }
+        public bool DeleteKey(string key)
+        {   
+            return _database.KeyDelete(key);
+        }
+
+        public bool KeyExists(string key)
+        {
+            return _database.KeyExists(key);
+        }
+    }
+    public interface IRedisDB 
+    {
+        void SetString(string key, string value, TimeSpan? expiry = null);
+        string GetString(string key);
+        bool DeleteKey(string key);
+        bool KeyExists(string key);
+    }
+}
