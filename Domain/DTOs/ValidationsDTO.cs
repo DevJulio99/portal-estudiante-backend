3d6440f114b6999c4bc2575a2243b216f53701fa
devjulio26 <devjulio26@gmail.com>
2025-04-23 14:35:41 -0500

    implementacion de captcha para login

---
+++ Domain/DTOs/ValidationsDTO.cs
@@ -0,0 +1,8 @@
+namespace MyPortalStudent.Domain.DTOs
+{
+    public class ApiValidation
+    {
+        public List<string> Messages { get; set; } = [];
+        public string field { get; set; } = "";
+    }
+}
