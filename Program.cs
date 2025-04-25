4d793a4965718fab79dfc95466ff4f5a832b6893
DevJulio99 <devjulio26@gmail.com>
2025-04-24 19:02:42 -0500

    Revert "Merge pull request #13 from DevJulio99/feat/aplicar-cambios"
    This reverts commit 7263216e31f28e650149d431eb728e9e62e2ee40, reversing
    changes made to 7dce15e84edc610b398850a6874091e650c5a89a.

--- Program.cs
+++ Program.cs
@@ -57,6 +57,9 @@ builder.Services.AddSwaggerGen(
             Description = "Servicio."
         }
         );
+        // var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        // options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
     }
 );
 
