2297f5dc4567898958bab7f02d7a6cbf4a3dbbb0
devjulio26 <devjulio26@gmail.com>
2025-11-10 17:15:00 -0500

    cambios para preguntas de competencias

--- Domain/DTOs/CompetenciasGenerales/ListaEstadoCompetenciaDTO.cs
+++ Domain/DTOs/CompetenciasGenerales/ListaEstadoCompetenciaDTO.cs
@@ -4,5 +4,7 @@ namespace MyPortalStudent.Domain.DTOs.CompetenciasGenerales
     {
         public string tiempoIniciado { get; set; }
         public string tiempoFinalizado { get; set; }
+        public int ultimaPregunta { get; set; }
+        public int tiempoUltimaPregunta { get; set; }
     }
 }
--- Domain/IServices/ICompetenciasGeneralesService.cs
+++ Domain/IServices/ICompetenciasGeneralesService.cs
@@ -16,7 +16,7 @@ namespace MyPortalStudent.Domain.IServices
         Task<Boolean> registrarActividadPostulante(UltimaActividadDTO ultimaActividadDto);
         Task<List<UltimaRespuestaExamenDTO>> getUltimaRespuestaExamen(int idPostulante, int idCompetencia);
         //Task<string> getUltimaActividadPostulante(int idPostulante, int idCompetencia);
-        Task<List<ListaEstadoCompetenciaDTO>> listarEstadoCompetencia(int idPostulante, int? idCompetencia);
+        Task<List<ListaEstadoCompetenciaDTO>> listarEstadoCompetencia(int idPostulante, int idCompetencia);
         Task<Boolean> registrarEstadoCompetencia(EstadoCompetenciaDTO estadoCompetenciaDto);
         Task<Boolean> actualizarEstadoCompetencia(EstadoCompetenciaDTO estadoCompetenciaDto);
         Task<Boolean> alumnoHabilitado(string? dniAlumno);
